0fc02000fabfddafef4aa343314a97827feabe21
zx69 <611411281@qq.com>
2022-10-08 18:40:08 +0800

    💡 add comment

--- src/views/builder/editor/typings.ts
+++ src/views/builder/editor/typings.ts
@@ -9,54 +9,51 @@ export type PanelConfig = {
   panelType?: string,
 };
 
+// 元素节点类型
 export type SchemaType = 'module' | 'block' | 'component';
+// 通用的元素节点定义
 export type CommonCompProp<T extends SchemaType = SchemaType> = {
-  index: T extends 'module' ? number : undefined, // 在模块列表中的排列序号
+  // 元素类型定义
   type: T,
+  // 模块数据，仅type=module
+  data: T extends 'module' ? {
+    [key: string]: unknown,
+  } : undefined,
+  // 元素类型，仅type=component
+  component: T extends 'component' ? ((keyof typeof moduleComponentsMap) | ((data: Obj, attrs: Obj) => VNode)) : undefined,
+  // 元素节点的props，仅type=component
+  props: T extends 'component' ? Obj : undefined,
   class?: string,
   style?: {
     [key: string]: string,
   },
+  // 用户自定义的style, 只在编辑时使用, 预览时合并入style再渲染
   customStyle?: {
     [key: string]: string,
   },
+  // H5端响应式定义，可包含CommonCompProp的任一属性，在H5端时将覆盖相应的同名属性
   mobile?: CommonCompProp<T>,
+  // 微信端响应式定义，可包含CommonCompProp的任一属性，在微信端时将覆盖相应的同名属性
   weixin?: CommonCompProp<T>,
+  // 当前元素可激活的操作面板
   operation?: string | false,
+  // 在模块列表中的排列序号，仅type=module
+  index: T extends 'module' ? number : undefined,
+  // section ID，仅type=module
   sid: T extends 'module' ? string : undefined,
+  // module ID，仅type=module
   mid: T extends 'module' ? string : undefined,
+  // 模块高宽比，仅type=module
   aspectRadio: T extends 'module' ? string : undefined,
+  // 子元素，仅type!=component
   children: T extends 'component' ? undefined : (string | (CompBlock | CompComponent)[]),
-  data: T extends 'module' ? {
-    // status: 'edit' | 'preview',
-    [key: string]: unknown,
-  } : undefined,
-  component: T extends 'component' ? ((keyof typeof moduleComponentsMap) | ((data: Obj, attrs: Obj) => VNode)) : undefined,
-  props: T extends 'component' ? Obj : undefined,
 };
 
 export type CompModule = CommonCompProp<'module'>;
-// export type CompModule = CommonCompProp<'module'> & {
-//   index: number, // 在模块列表中的排列序号
-//   type: 'module',
-//   mid: string,
-//   // sid?: string,
-//   aspectRadio?: string,
-//   children?: string | (CompBlock | CompComponent)[]
-//   data: {
-//     // status: 'edit' | 'preview',
-//     [key: string]: unknown,
-//   },
-// };
+
 export type CompBlock = CommonCompProp<'block'>;
-// export type CompBlock = CommonCompProp<'block'> & {
-//   type: 'block',
-// };
+
 export type CompComponent = CommonCompProp<'component'>;
-// export type CompComponent = CommonCompProp<'component'> & {
-//   component: string | ((data: Obj, attrs: Obj) => VNode),
-//   props: Obj,
-// };
 
 
 export type PreviewMode = 'pc' | 'mobile'; // 预览模式切换
--- src/views/builder/editor/utils/compiler.ts
+++ src/views/builder/editor/utils/compiler.ts
@@ -34,6 +34,7 @@ const mobileSchemaOverwrite = (nodeSchema: CommonCompProp, previewMode?: Preview
   return _nodeSchema;
 };
 
+// 类型保护
 function isModule(schema: CommonCompProp): schema is CommonCompProp<'module'> {
   return schema.type === 'module';
 }
@@ -44,9 +45,7 @@ function isBlock(schema: CommonCompProp): schema is CommonCompProp<'block'> {
   return schema.type === 'block';
 }
 
-/**
- * 解析字符串模板表达式
- */
+// 解析字符串模板表达式
 export const parseTplExpress = (data: Obj, express: string) => {
   if (!data || !express) {
     return '';
@@ -67,7 +66,7 @@ const parseStyleValue = (styleObj: Obj, data: Obj) => {
   return _style;
 };
 
-// 规范化nodeSchema策略集合
+// 规范化nodeSchema策略集合（策略模式）
 const normalizeStrategies = {
   module(nodeSchema: CommonCompProp) {
     return {
@@ -94,10 +93,7 @@ const normalizeStrategies = {
   },
 };
 
-
-/**
- * 合并attrs
- */
+// 合并attrs
 const mergeAttrs = (baseAttrs: Obj, appendAttrs: Obj) => {
   // console.log(baseAttrs, appendAttrs);
   Object.entries(appendAttrs).forEach(([key, val]) => {
@@ -122,10 +118,10 @@ const mergeAttrs = (baseAttrs: Obj, appendAttrs: Obj) => {
 let globalIdCount = 1; // 用于生成fid的计算器
 
 const comilpeSchema = (
-  nodeSchema: CommonCompProp<SchemaType>,
-  renderData?: Obj,
-  parentAttrs?: Obj | null,
-  config: { mode: PreviewMode, status: RenderStatus } = { mode: 'pc', status: 'preview' },
+  nodeSchema: CommonCompProp<SchemaType>, // schema
+  renderData?: Obj, // module的data
+  parentAttrs?: Obj | null, // 父节点传入子节点的属性
+  config: { mode: PreviewMode, status: RenderStatus } = { mode: 'pc', status: 'preview' }, // 配置选项
 ): VNode => {
   if (typeof nodeSchema !== 'object') {
     throw Error('render json schema must be a object!');
@@ -135,14 +131,16 @@ const comilpeSchema = (
   if (nodeSchema.sid) {
     globalIdCount = 1;
   }
-
+  // 根据platform合并样式
   nodeSchema = mobileSchemaOverwrite(nodeSchema, config.mode);
 
   // 没有找到规范策略时则设type为最基本类型block
   if (!Object.keys(normalizeStrategies).includes(nodeSchema.type)) {
     nodeSchema.type = 'block';
   }
+  // 规范化策略
   const normalizeStrategy = normalizeStrategies[nodeSchema.type];
+  // schema规范化
   const vnode = normalizeStrategy(nodeSchema, config.status);
 
   // 模块汇总的data
@@ -167,6 +165,7 @@ const comilpeSchema = (
     Object.assign(nodeSchema.style, nodeSchema.customStyle);
   }
 
+  // fid：可编辑元素都增加该id,用于识别当前选中元素
   let fid: string | undefined;
   const parentFid = nodeSchema.sid ?? parentAttrs?.parentFid;
   // let componentNode: ComponentOptionsMixin | undefined;
@@ -190,7 +189,6 @@ const comilpeSchema = (
     attrs.style = parseStyleValue(attrs.style, moduleData);
   }
 
-  // console.log(attrs);
   // 子元素为组件时
   if (isComponent(nodeSchema)) {
     if (typeof nodeSchema.component === 'function') {
